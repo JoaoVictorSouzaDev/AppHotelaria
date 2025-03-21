fa4d2da357528f7ac21a17d7ebae4fae4924501b
JOAO VICTOR DE SOUZA <joao.vsouza30@senacsp.edu.br>
2025-03-21 17:01:14 -0300

    otimização do código

--- src/view/CadCliente.java
+++ src/view/CadCliente.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -52,16 +53,25 @@ public class CadCliente extends Application {
         txtNome.setMaxWidth(200);
 
         Label lblCpf = new Label("CPF");
-        TextField txtCpf = new TextField();
+        TextField txtCpf = createMaskField("###.###.###-##");
         txtCpf.setMaxWidth(200);
 
         Label lblTelefone = new Label("Telefone");
-        TextField txtTelefone = new TextField();
+        TextField txtTelefone = createMaskField("(##) #####-####");
         txtTelefone.setMaxWidth(200);
 
         Label lblEmail = new Label("Email");
         TextField txtEmail = new TextField();
-        txtEmail.setMaxWidth(200);
+        txtEmail.setMinWidth(200);
+
+        ComboBox emailOpt = new ComboBox();
+        emailOpt.getItems().addAll("@gmail.com", "@email.com","@yahoo.com");
+        emailOpt.setMinWidth(120);
+
+        GridPane gridEmail = new GridPane();
+        gridEmail.add(txtEmail, 1, 0);
+        gridEmail.add(emailOpt, 2, 0);
+        gridEmail.setMaxWidth(200);
 
         GridPane fromGrid = new GridPane();
         fromGrid.setAlignment(Pos.CENTER);
@@ -72,51 +82,54 @@ public class CadCliente extends Application {
         fromGrid.add(lblTelefone, 0, 2);
         fromGrid.add(txtTelefone, 1, 2);
         fromGrid.add(lblEmail, 0, 3);
-        fromGrid.add(txtEmail, 1, 3);
+        fromGrid.add(gridEmail, 1, 3);
         fromGrid.setHgap(10);
         fromGrid.setVgap(10);
 
         Button btnCadastrar = new Button("Cadastrar", iconCadastrar);
         btnCadastrar.setMaxWidth(105);
-        btnCadastrar.setStyle("-fx-background-color: #006039");
         Button btnAtualizar = new Button("Atualizar",iconAtualizar);
         btnAtualizar.setMaxWidth(105);
-        btnAtualizar.setStyle("-fx-background-color: #006039");
         Button btnExcluir = new Button("Excluir",iconExcluir);
         btnExcluir.setMaxWidth(105);
-        btnExcluir.setStyle("-fx-background-color: #006039");
 
         HBox buttonsBox = new HBox (btnAtualizar, btnCadastrar, btnExcluir);
+        buttonsBox.setPadding(new Insets(20,20,20,20));
         buttonsBox.setSpacing(20);
         buttonsBox.setAlignment(Pos.CENTER);
 
-        VBox mainCad = new VBox(tituloBox, fromGrid, buttonsBox);
-        mainCad.setSpacing(10);
-        mainCad.setAlignment(Pos.CENTER);
-
-        VBox rectangleCad = new VBox(mainCad);
-        rectangleCad.setAlignment(Pos.CENTER);
-        rectangleCad.setStyle(
-                "-fx-background-color: #ffffff;-fx-background-radius: 30"
-        );
-        rectangleCad.setMaxHeight(500);
-        rectangleCad.setMinWidth(400);
-
-        HBox mainContent = new HBox(rectangleCad);
-        mainContent.setMaxWidth(1200);
-        mainContent.setMaxHeight(700);
-        mainContent.setAlignment(Pos.CENTER_RIGHT);
-
-        VBox layout = new VBox();
-        layout.setStyle("-fx-background-image: url('BackGround.jpeg'); -fx-background-repeat: stretch; -fx-background-size: cover;");
+        VBox layout = new VBox(tituloBox, fromGrid, buttonsBox);
         layout.setAlignment(Pos.CENTER);
         StackPane root = new StackPane();
-        root.getChildren().addAll(layout, mainContent);
+        root.getChildren().addAll(layout);
 
         Scene scene = new Scene(root, 500,700);
         stage.setTitle("Cadastro de Cliente");
         stage.setScene (scene);
         stage.show();
+    }
+
+    private TextField createMaskField(String mask) {
+        TextField txtMask = new TextField();
+        txtMask.textProperty().addListener((observable, oldValue, newValue) -> {
+            String value = newValue.replaceAll("[^0-9]", "");
+            StringBuilder formatField = new StringBuilder();
+            int index = 0;
+            for (char caracter : mask.toCharArray()) {
+                if (caracter == '#') {
+                    if (index < value.length()) {
+                        formatField.append(value.charAt(index));
+                        index++;
+                    } else {
+                        break;
+                    }
+                } else {
+                    formatField.append(caracter);
+                }
+            }
+            txtMask.setText(formatField.toString());
+        });
+        return txtMask;
     }
 
     public static void main(String[] args) {
