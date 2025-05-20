b0fc172a090a62c7bb1281e7fcaba66de86de87b
Joao Souza <joaovictorsouza3233@gmail.com>
2025-05-20 17:18:52 -0300

    Atualização Conexão

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -6,17 +6,17 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Hoteldb";
-    private static final String USUARIO = "dbaHotel";
-    private static final String SENHA = "dbaHotel123";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/Hoteldb";
+    private static final String usuario = "dbaHotel";
+    private static final String senha = "dbaHotel123";
 
     public Connection conectar() {
         Connection condb = null;
 
         try {
-            Class.forName(DRIVER);
-            condb = DriverManager.getConnection(URL, USUARIO, SENHA);
+            Class.forName(driver);
+            condb = DriverManager.getConnection(url, usuario, senha);
             return condb;
         } catch (ClassNotFoundException e) {
             System.err.println("Erro: Driver JDBC não encontrado. " + e.getMessage());
